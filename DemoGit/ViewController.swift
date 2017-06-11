e556f861a48af9a2ccfd0512409ef26945c29077
Shikha Singla <shikha.singla@a1-t-d05.local>
2017-06-11 11:46:59 +0530

    "NewChanges"

--- DemoGit/ViewController.swift
+++ DemoGit/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     var str = String()
+    var str1 = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         str = "Shikha"
         print(str)
+        str1 = "shemona"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
